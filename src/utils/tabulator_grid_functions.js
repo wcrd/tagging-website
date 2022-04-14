b2558c164c83bbd150922e50342f0b8b9c00e77c
wcrd <11898748+wcrd@users.noreply.github.com>
2022-04-14 11:23:41 -0600

    - Update grid and add data methods - Add filters

--- src/utils/tabulator_grid_functions.js
+++ src/utils/tabulator_grid_functions.js
@@ -1,3 +1,42 @@
+
+
+// import pointsData from '../data/points/latest/web_data.json'
+
+// 
+//  FORMATTERS 
+// 
+
+// Custom icon formatter
+export const copyIcon = (cell, formatterParams) => "<i class='copy-icon'></i>"
+
+
+
+// 
+//  LOADERS 
+// 
+
+// // Loads tag data from file, loads it into the given table, and redraws.
+// export async function setTableData(table){
+//     const data = await loadPointsData()
+//     await table.setData(data)
+
+//     table.redraw()
+// }
+
+
+// // Not needed when using Vue
+// async function loadPointsData(){
+//     const r = await fetch('data/points/latest/web_data.json');
+//     const data = await r.json()
+
+//     return data
+// }
+
+
+// 
+//  TESTING
+// 
+
 export function testFunction(){
     console.log("I'm a test function")
 }
