b1301d39988723d77aa11ad53de8e09548375929
wcrd <11898748+wcrd@users.noreply.github.com>
2022-04-14 08:56:05 -0600

    Update nav header

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -2,14 +2,14 @@
   <div class="bg-indigo-500 h-10 px-5 flex justify-between items-center text-white">
     <div id="header-left">
       <div id="header-title">
-        Point Tagging Guide
+        Building Tagging Guide
       </div>
     </div>
     <div id="header-right" class="flex flex-row space-x-2">
-      <div><router-link to="/">Points Guide</router-link></div>
-      <div><router-link to="/test">Equipment Types</router-link></div>
-      <div>Equipment Diagrams</div>
-      <div>Modelling Documentation</div>
+      <div><router-link to="/">Points</router-link></div>
+      <div><router-link to="/test">Equipment</router-link></div>
+      <div>Docs &amp; Guides</div>
+      <div>About</div>
     </div>
   </div>
 </template>
